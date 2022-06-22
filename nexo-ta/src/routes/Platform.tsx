edaeeb01e9cc2018e042176f367e82ff1e324e5b
bandrewk <bryan.king@fourthquarter.eu>
2022-06-22 14:54:38 +0200

    Setup platform routing

--- nexo-ta/src/routes/Platform.tsx
+++ nexo-ta/src/routes/Platform.tsx
@@ -1,11 +1,22 @@
+import { useLocation, Route, BrowserRouter, Routes } from "react-router-dom";
 import Sidebar from "../components/Sidebar/Sidebar";
+import SidebarMenuItem from "../components/Sidebar/SidebarMenuItem";
 import classes from "./Platform.module.css";
 
 const Platform = () => {
+  let location = useLocation();
+
   return (
     <div className={classes.platform}>
       <Sidebar />
-      <p>Content</p>
+      <p>
+        <Routes>
+          <Route path="/home" element={<p>Home</p>} />
+          <Route path="/overview" element={<p>Overview</p>} />
+          <Route path="/coinlist" element={<p>Coinlist</p>} />
+          <Route path="/transactions" element={<p>Transactions</p>} />
+        </Routes>
+      </p>
     </div>
   );
 };
