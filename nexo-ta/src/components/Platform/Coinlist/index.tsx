faa6ea4207780750afe6b5dd7f923147331d76c0
bandrewk <bryan.king@fourthquarter.eu>
2022-08-01 20:02:44 +0200

    Added option to hide small balances (default on)

--- nexo-ta/src/components/Platform/Coinlist/index.tsx
+++ nexo-ta/src/components/Platform/Coinlist/index.tsx
@@ -2,9 +2,12 @@ import HeadingPrimary from "../../UI/Text/HeadingPrimary";
 import classes from "./index.module.css";
 import CoinlistItem from "./CoinlistItem";
 import { useAppSelector } from "../../../hooks";
+import { useState } from "react";
 
 const Coinlist = () => {
   const currencies = useAppSelector((state) => state.currencies);
+
+  const [hideSmallBalances, setHideSmallBalances] = useState(true);
 
   return (
     <>
@@ -12,8 +15,23 @@ const Coinlist = () => {
       <p>Your portfolio at a glance!</p>
       <br />
 
+      <div className={classes.settings}>
+        <label className={classes.inputLabel}>
+          <input
+            type="checkbox"
+            checked={hideSmallBalances}
+            onChange={setHideSmallBalances.bind(null, !hideSmallBalances)}
+          />
+          Hide small balances
+        </label>
+      </div>
+
       <div className={classes["coinlist-container"]}>
         {currencies.map((cur) => {
+          if (hideSmallBalances) {
+            if (cur.amount >= 0 && cur.amount < 0.1) return;
+            if (cur.amount < 0 && cur.amount > -0.1) return;
+          }
           return (
             <CoinlistItem
               symbol={cur.symbol}
