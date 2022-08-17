c9bc9a9a95f0c6877309647f82dde42db1984cbb
bandrewk <6756767+bandrewk@users.noreply.github.com>
2022-08-17 15:08:23 +0200

    Added portfolio value and earned interest graphs

--- nexo-ta/src/components/Platform/Overview.tsx
+++ nexo-ta/src/components/Platform/Overview.tsx
@@ -1,37 +1,127 @@
 import HeadingPrimary from "../UI/Text/HeadingPrimary";
 import classes from "./Overview.module.css";
+import {
+  ResponsiveContainer,
+  Tooltip,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Legend,
+  Bar,
+  RadarChart,
+  PolarGrid,
+  PolarRadiusAxis,
+  PolarAngleAxis,
+  Radar,
+  LineChart,
+  Line,
+} from "recharts";
+import { useAppSelector } from "../../hooks";
 
 const Overview = () => {
+  const currencies = useAppSelector((state) => state.currencies);
+  const statistics = useAppSelector((state) => state.statistics);
+
+  let portfolioValue = 0;
+
+  const portfolioDistribution = currencies
+    .filter((item) => {
+      // Filter out small values and unsuported entries
+      if (item.amount < 0.01) return false;
+      if (!item.supported) return false;
+
+      // Update portfolio value
+      portfolioValue += item.usdEquivalent;
+
+      return true;
+    })
+    .map((item) => {
+      return { name: item.symbol, USD: item.usdEquivalent.toFixed(2) };
+    });
+
   return (
     <>
       <HeadingPrimary text="Overview" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eligendi
-        architecto, ab qui nam maiores pariatur deleniti aut molestias sint
-        nesciunt porro tempora minima nisi voluptas harum exercitationem dicta
-        consectetur blanditiis quasi neque repellendus. Molestias quibusdam
-        modi, voluptates mollitia magnam quaerat quae nihil pariatur corrupti
-        inventore magni tempora repudiandae minima reiciendis qui dolorem soluta
-        placeat laborum eveniet! Natus quae aspernatur eveniet quis laudantium
-        debitis atque ex officiis asperiores hic. Placeat recusandae repudiandae
-        velit quo nobis rerum expedita ea architecto illum! Iste ducimus ex
-        facere, qui rerum quia, nostrum totam temporibus quis maiores inventore
-        fugiat velit assumenda. Dicta velit quibusdam voluptatem!
-      </p>
+      <p>Visualization of your portfolio.</p>
       <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eligendi
-        architecto, ab qui nam maiores pariatur deleniti aut molestias sint
-        nesciunt porro tempora minima nisi voluptas harum exercitationem dicta
-        consectetur blanditiis quasi neque repellendus. Molestias quibusdam
-        modi, voluptates mollitia magnam quaerat quae nihil pariatur corrupti
-        inventore magni tempora repudiandae minima reiciendis qui dolorem soluta
-        placeat laborum eveniet! Natus quae aspernatur eveniet quis laudantium
-        debitis atque ex officiis asperiores hic. Placeat recusandae repudiandae
-        velit quo nobis rerum expedita ea architecto illum! Iste ducimus ex
-        facere, qui rerum quia, nostrum totam temporibus quis maiores inventore
-        fugiat velit assumenda. Dicta velit quibusdam voluptatem!
-      </p>
+      {/****************************************************************
+       * Portfolio value
+       ***************************************************************/}
+      {portfolioDistribution && portfolioDistribution.length && (
+        <div className={classes["chart-row"]}>
+          <h2>Portfolio value</h2>
+
+          <div className={classes["chart-row--section1"]}>
+            <p>{portfolioValue.toFixed(2)}$</p>
+
+            <ResponsiveContainer width="100%" height={250}>
+              <RadarChart
+                cx="50%"
+                cy="50%"
+                outerRadius="80%"
+                data={portfolioDistribution}
+              >
+                <PolarGrid />
+                <PolarAngleAxis dataKey="name" />
+                <PolarRadiusAxis />
+                <Radar
+                  name="Portfolio"
+                  dataKey="USD"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                  fillOpacity={0.6}
+                />
+              </RadarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      )}
+
+      {/****************************************************************
+       * Earned interest graph
+       ***************************************************************/}
+
+      {statistics.interestData && statistics.interestData.length && (
+        <div className={classes["chart-row"]}>
+          <h2>Earned interest</h2>
+          <ResponsiveContainer width="100%" height={300}>
+            <LineChart
+              width={500}
+              height={300}
+              data={statistics.interestData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="date"
+                ticks={[
+                  statistics.interestData[0].date,
+                  statistics.interestData[statistics.interestData.length - 1]
+                    .date,
+                ]}
+                padding={{ left: 20, right: 20 }}
+              />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+
+              <Line
+                type="monotone"
+                dataKey="value"
+                stroke="#82ca9d"
+                dot={false}
+                strokeWidth={2}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      )}
     </>
   );
 };
