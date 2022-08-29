532c9d326b477642cec28a5462ec1e2792c8cc01
bandrewk <6756767+bandrewk@users.noreply.github.com>
2022-08-29 11:14:25 +0200

    Added bank of England api endpoint

--- nexo-ta/src/config.ts
+++ nexo-ta/src/config.ts
@@ -26,6 +26,26 @@ export const ECB_API_EUR = (startDate: string, endDate: string) => {
   return api;
 };
 
+// Bank of england api for GBP
+// Sadly unuseable without CORS proxy
+// Date format: 28/Aug/2022
+export const BOE_API_GBP = (date: string) => {
+  const api =
+    "https://crossorigin.me/https://www.bankofengland.co.uk/boeapps/database/_iadb-fromshowcolumns.asp?csv.x=yes";
+
+  const params = new URLSearchParams({
+    Datefrom: `${date}`,
+    Dateto: `${date}`,
+    SeriesCodes: "XUDLGBD",
+    CSVF: "TN",
+    UsingCodes: "Y",
+    VPD: "Y",
+    VFD: "N",
+  });
+
+  return api + params;
+};
+
 //Refresh news  every n seconds (ms)
 export const NEWSFEED_PULL_RATE = 300000; // 5min
 
